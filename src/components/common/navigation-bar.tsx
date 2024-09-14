d34a6120fc7d5f8916ee286c69e6adfe85607fca
ImGabreuw <imgabreuw.dev@gmail.com>
2024-09-14 12:56:23 -0300

    refactor: add logo and change navigation links

--- src/components/common/navigation-bar.tsx
+++ src/components/common/navigation-bar.tsx
@@ -9,6 +9,8 @@ import {
 } from "@/components/ui/navigation-menu";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 
+import { Logo } from "./logo";
+
 export function NavigationBar() {
   return (
     <header className="flex h-20 w-full items-center px-4 md:px-6">
@@ -16,13 +18,13 @@ export function NavigationBar() {
         <SheetTrigger asChild>
           <Button variant="outline" size="icon" className="lg:hidden">
             <MenuIcon className="h-6 w-6" />
-            <span className="sr-only">Toggle navigation menu</span>
+            <span className="sr-only">Alternar menu de navegação</span>
           </Button>
         </SheetTrigger>
         <SheetContent side="left">
           <Link href="#" prefetch={false}>
-            <MountainIcon className="h-6 w-6" />
-            <span className="sr-only">Acme Inc</span>
+            <Logo size={48} />
+            <span className="sr-only">3A</span>
           </Link>
           <div className="grid gap-2 py-6">
             <Link
@@ -30,35 +32,28 @@ export function NavigationBar() {
               className="flex w-full items-center py-2 text-lg font-semibold"
               prefetch={false}
             >
-              Home
+              Início
             </Link>
             <Link
               href="#"
               className="flex w-full items-center py-2 text-lg font-semibold"
               prefetch={false}
             >
-              About
+              Curso
             </Link>
             <Link
               href="#"
               className="flex w-full items-center py-2 text-lg font-semibold"
               prefetch={false}
             >
-              Services
-            </Link>
-            <Link
-              href="#"
-              className="flex w-full items-center py-2 text-lg font-semibold"
-              prefetch={false}
-            >
-              Contact Us
+              Relatório
             </Link>
           </div>
         </SheetContent>
       </Sheet>
       <Link href="#" className="mr-6 hidden lg:flex" prefetch={false}>
         <MountainIcon className="h-6 w-6" />
-        <span className="sr-only">Acme Inc</span>
+        <span className="sr-only">3A</span>
       </Link>
       <NavigationMenu className="hidden lg:flex">
         <NavigationMenuList>
@@ -68,7 +63,7 @@ export function NavigationBar() {
               className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
               prefetch={false}
             >
-              Home
+              Início
             </Link>
           </NavigationMenuLink>
           <NavigationMenuLink asChild>
@@ -77,7 +72,7 @@ export function NavigationBar() {
               className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
               prefetch={false}
             >
-              About
+              Curso
             </Link>
           </NavigationMenuLink>
           <NavigationMenuLink asChild>
@@ -86,22 +81,13 @@ export function NavigationBar() {
               className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
               prefetch={false}
             >
-              Services
-            </Link>
-          </NavigationMenuLink>
-          <NavigationMenuLink asChild>
-            <Link
-              href="#"
-              className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
-              prefetch={false}
-            >
-              Contact Us
+              Relatórios
             </Link>
           </NavigationMenuLink>
         </NavigationMenuList>
       </NavigationMenu>
       <div className="ml-auto">
-        <Button>Login</Button>
+        <Button>Entrar</Button>
       </div>
     </header>
   );
