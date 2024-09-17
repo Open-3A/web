c0ed27a0cbefcb9a8431898f3e23c83629fdccdb
ImGabreuw <imgabreuw.dev@gmail.com>
2024-09-16 22:19:39 -0300

    fix: remove unused mock data

--- src/components/report/report-section.tsx
+++ src/components/report/report-section.tsx
@@ -5,30 +5,6 @@ import { H2 } from "../typography/h2";
 import { Lead } from "../typography/lead";
 import { ReportCard } from "./report-card";
 
-const reportsData = [
-  {
-    id: 1,
-    title: "Análise Técnica - Setembro",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt unde quam fugiat sint, porro aliquid, dignissimos esse pariatur, maxime vel quod perferendis! Ipsam, excepturi eos!",
-    publishedAt: "12/09/2024",
-  },
-  {
-    id: 2,
-    title: "Previsão Econômica Q3",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt unde quam fugiat sint, porro aliquid, dignissimos esse pariatur, maxime vel quod perferendis! Ipsam, excepturi eos!",
-    publishedAt: "10/09/2024",
-  },
-  {
-    id: 3,
-    title: "Relatório de Dividendos",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt unde quam fugiat sint, porro aliquid, dignissimos esse pariatur, maxime vel quod perferendis! Ipsam, excepturi eos!",
-    publishedAt: "05/09/2024",
-  },
-];
-
 export async function ReportSection() {
   const reports = await getReports(0);
 
